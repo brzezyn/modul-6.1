c12becec8fa916eabf413a3b774dd2680cf55730
Artur Brzeziński <arturitu.xdd@gmail.com>
2021-02-13 17:38:08 +0100

    JDBC

---
+++ kodilla-testing/src/test/java/com/kodilla/testing/loop/testLoopAndConditionalBreakpoints.java
@@ -0,0 +1,20 @@
+package com.kodilla.testing.loop;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class testLoopAndConditionalBreakpoints {
+    @Test
+    public void testLoop() {
+        //GIVEN
+        long sum = 0;
+        //WHEN
+        for(int n = 0; n < 1000; n++) {
+            sum += n;
+            System.out.println("[" + n + "] Sum equals: " + sum);
+        }
+        //THEN
+        assertEquals(499500, sum);
+    }
+}
